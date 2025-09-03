4f0532d5d59b45c6dc31c3bc2b149d3624da743b
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2025-09-03 09:46:03 +0300

    basic lambda

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    print("Received event:", json.dumps(event, indent=2))
+    print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Function executed successfully!')
+        'body': json.dumps({"message": "Hello from your Lambda function!"})
     }
